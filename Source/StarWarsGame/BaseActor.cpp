47c9ec69a963b3636c421f94f003651b1521dd23
Neha Ghanta <neha.ghanta@gmail.com>
2021-04-23 21:15:12 -0400

    Attack3 and Saber Collision
    Added 3rd attack to combo and basic collision detection for saber

---
+++ Source/StarWarsGame/BaseActor.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseActor.h"
+#include "Components/SkeletalMeshComponent.h"
+
+// Sets default values
+ABaseActor::ABaseActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// creating skeletal mesh component
+	CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMeshComponent"));
+	SetRootComponent(SkeletalMeshComponent);
+}
+
+// Called when the game starts or when spawned
+void ABaseActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABaseActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
